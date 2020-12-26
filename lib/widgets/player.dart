e78f9d8e33b3107604cb0e746f615b5e132336d0
dahef34 <52498723+dahef34@users.noreply.github.com>
2020-12-26 14:05:52 -0500

    removed

--- lib/widgets/player.dart
+++ lib/widgets/player.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/widgets/featured.dart';
 
 class VideoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: <Widget>[]));
+    return Scaffold(body: Column(children: <Widget>[
+      Spacer(),
+      Featured(),
+      Spacer()
+    ]));
   }
 }
